b4698de5f3cff0038f7be22cfb1d7fa6e8f44cc4
ansarmaq <P14152540@myemail.dmu.ac.uk>
2017-03-10 15:16:07 +0000

    commit 16
    another js was added to the travel section. this will have a counter. in
    10 secs it will reach the max numbers added.

--- Responsive/js/custom.js
+++ Responsive/js/custom.js
@@ -170,6 +170,24 @@ $(document).ready(function() {
     
 });
 
+// Add counter
+$(document).ready(function() {
+    
+    $('.counter-num').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+// Add animation/ Initialize Woo
+$(document).ready(function() {
+    
+    'use strict';
+   
+    new WOW().init();
+    
+});
+
 
 
 
