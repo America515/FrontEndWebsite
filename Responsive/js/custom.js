bfac18a754b318184edb090f149a5c5487a7d9a9
ansarmaq <P14152540@myemail.dmu.ac.uk>
2017-03-10 15:13:29 +0000

    commit 15
    added jquery to the website. This will allow the images to have slider
    and also buttons.

--- Responsive/js/custom.js
+++ Responsive/js/custom.js
@@ -156,6 +156,20 @@ $(document).ready(function() {
     
 });
 
+// Add bx slider to travel
+$(document).ready(function() {
+    
+    $('.bxslider').bxSlider({
+        
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1, 
+        maxSlides: 3, 
+        slideMargin: 50
+    });
+    
+});
+
 
 
 
